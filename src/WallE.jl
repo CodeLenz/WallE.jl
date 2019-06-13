9233bcba60acd3e281ae8e47c5f263e2994c8865
Eduardo Lenz <eduardobarplenz@gmail.com>
2019-06-13 10:44:47 -0300

    Adding progressmeter

--- src/WallE.jl
+++ src/WallE.jl
@@ -3,7 +3,7 @@
 #
 module WallE
 
-using LinearAlgebra
+using LinearAlgebra, ProgressMeter
 
 export Wall_E2
 
@@ -276,7 +276,7 @@ function Wall_E2(f::Function, df::Function, x0::Array{Float64},
   delta_M = Float64[]
 
   ####################### LOOP PRINCIPAL #######################
-  tempo = @elapsed for iter=1:niter
+  tempo = @elapsed @showprogress 1 "Minimizing..." for iter=1:niter
 
       # Calcula a derivada de f, chamando df(x)
       D .= df(x0)
