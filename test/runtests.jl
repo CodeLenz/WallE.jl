bbdc4a051694fcd1375b15d0a9ac7dae858f34e5
CodeLenz <eduardobarplenz@gmail.com>
2019-08-27 17:59:48 -0300

    A little bit to the rigth

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,10 +6,10 @@ using Test
 # Run tests
  
 
-println("#"^40, "\n  Test 1\n","#"^40)
+println("#"^40, "\n"," "^30,"  Test 1\n","#"^40)
 
 @time include("test_unconstrained.jl")
 
-println("#"^40, "\n  Test 2\n","#"^40)
+println("#"^40, "\n"," "^30,"  Test 2\n","#"^40)
 
 @time include("test_constrained.jl")
