2f524e2f18ce53b92a0a73b962b6dcc75911fb57
Eduardo Lenz <eduardobarplenz@gmail.com>
2019-08-30 16:05:25 -0300

    Testing modified Conjugate Descent

--- src/WallE.jl
+++ src/WallE.jl
@@ -388,7 +388,7 @@ module WallE
              #
              beta_f = 0.0
              if length(free_x)>0 
-                beta_f = dot(D[free_x]-Da[free_x],D[free_x]) / dot(D[free_x]-Da[free_x],da[free_x])
+                beta_f = -dot(D[free_x],D[free_x]) / dot(D[free_x],da[free_x])
              end
              
              @show beta_f
