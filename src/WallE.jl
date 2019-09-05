acaf40a7bdde685b3c80b0e55265e1e4bed6bc46
Eduardo Lenz <eduardobarplenz@gmail.com>
2019-09-05 13:04:09 -0300

    Armijos now receive external iteration counter

--- src/WallE.jl
+++ src/WallE.jl
@@ -689,7 +689,7 @@ module WallE
         # improve is a flag to indicate that the LS improved the solution
         # Iblock_m and I_block_M are the set of blocked (projected) variables
         x0, x1, α, αI, f0,  improved, blocked_changed, Iblock_m, Iblock_M = Armijo_Projected(x0,x1,f0,d,D,Da,
-                                                                               ci,cs,f,blocked_x,
+                                                                               ci,cs,f,blocked_x,iter,
                                                                                cut_factor,
                                                                                0.1,α_ini,α_min)
      
