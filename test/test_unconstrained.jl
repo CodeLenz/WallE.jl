0b2d89d89543e4c9cc20e57e9cea518c10a50497
Eduardo Lenz <eduardobarplenz@gmail.com>
2019-09-05 16:07:20 -0300

    Adding Goldstein Price unconstrained test

--- test/test_unconstrained.jl
+++ test/test_unconstrained.jl
@@ -94,7 +94,7 @@
     end
 
     # Ponto inicial
-    x0 = 10*rand(2)
+    x0 = [3.0 ; 0.5]
 
     # Restrições laterais
     ci = -Inf*ones(2)
@@ -113,6 +113,46 @@
     #show(IOContext(stdout, :compact => false, :limit => false), "text/plain", [x_opt [3.0 ; 5.0]])
     println("\n")
 
+
+
+
+
+   println("\n\n############\n  Test 1.4\n############")
+   # Goldstein-Price
+   # partida (-2,2) ou (2,-2). Interessante comparar.
+   # minimo em (0,-1), valendo 3.0
+
+    function f(x) 
+        ((x[2]+x[1]+1)^2*(3*x[2]^2+6*x[1]*x[2]-14*x[2]+3*x[1]^2-14*x[1]+19)+1)*((2*x[1]-3*x[2])^2*(27*x[2]^2-36*x[1]*x[2]+48*x[2]+12*x[1]^2-32*x[1]+18)+30)
+       
+    end
+
+       
+    function df(x)
+        df1 = ((x[2]+x[1]+1)^2*(3*x[2]^2+6*x[1]*x[2]-14*x[2]+3*x[1]^2-14*x[1]+19)+1)*(4*(2*x[1]-3*x[2])*(27*x[2]^2-36*x[1]*x[2]+48*x[2]+12*x[1]^2-32*x[1]+18)+(-36*x[2]+24*x[1]-32)*(2*x[1]-3*x[2])^2)+(2*(x[2]+x[1]+1)*(3*x[2]^2+6*x[1]*x[2]-14*x[2]+3*x[1]^2-14*x[1]+19)+(x[2]+x[1]+1)^2*(6*x[2]+6*x[1]-14))*((2*x[1]-3*x[2])^2*(27*x[2]^2-36*x[1]*x[2]+48*x[2]+12*x[1]^2-32*x[1]+18)+30)
+        df2 = ((x[2]+x[1]+1)^2*(3*x[2]^2+6*x[1]*x[2]-14*x[2]+3*x[1]^2-14*x[1]+19)+1)*((2*x[1]-3*x[2])^2*(54*x[2]-36*x[1]+48)-6*(2*x[1]-3*x[2])*(27*x[2]^2-36*x[1]*x[2]+48*x[2]+12*x[1]^2-32*x[1]+18))+(2*(x[2]+x[1]+1)*(3*x[2]^2+6*x[1]*x[2]-14*x[2]+3*x[1]^2-14*x[1]+19)+(x[2]+x[1]+1)^2*(6*x[2]+6*x[1]-14))*((2*x[1]-3*x[2])^2*(27*x[2]^2-36*x[1]*x[2]+48*x[2]+12*x[1]^2-32*x[1]+18)+30)
+        return [df1 ; df2]
+    end
+
+    # Ponto inicial
+    x0 = [-2;3]
+
+    # Restrições laterais
+    ci = -Inf*ones(2)
+    cs =  Inf*ones(2)
+
+    # Chama o otimizador
+    x_opt, flag, norma = WallE.Wall_E2(f,df,x0,ci,cs,1000)
+    x_opt_GC, flag, norma = WallE.Wall_E2(f,df,x0,ci,cs,1000,ENABLE_GC=true)
+ 
+
+    # The test
+    @test isapprox(x_opt,[0.0 ; -1.0],rtol=1E-2)
+    @test isapprox(x_opt_GC,[0.0 ; -1.0],rtol=1E-2)
+
+    #println("\n","# Resultado #")
+    #show(IOContext(stdout, :compact => false, :limit => false), "text/plain", [x_opt [3.0 ; 5.0]])
+    println("\n")
 
 
 
