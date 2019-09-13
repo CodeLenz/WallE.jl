d7bbd8f18081a2ef12a44d832a5fb03dc5fff90c
CodeLenz <eduardobarplenz@gmail.com>
2019-09-13 20:17:07 -0300

    Friday night,..

--- src/WallE.jl
+++ src/WallE.jl
@@ -557,7 +557,7 @@ function GC_projected!(d::Array{Float64},D::Array{Float64},last_D::Array{Float64
 
                  # Correction
                  #L .= L .+ effective_α.*Extract_as_vector(last_d,pos)
-                 L .= L .+ effective_α.*last_d[pos]*y
+                 L .= L .+ effective_α.*last_d[pos]*y/norm(x-last_x)
 
 
              end # effective_α
