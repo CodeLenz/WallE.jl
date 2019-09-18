940c35188f320313641f0a63530ed90d6c3e1135
Eduardo Lenz <eduardobarplenz@gmail.com>
2019-09-18 14:46:51 -0300

    removing QN from tests

--- test/test_unconstrained.jl
+++ test/test_unconstrained.jl
@@ -27,14 +27,12 @@
     # Chama o otimizador
     x_opt, f0, fopt, flag,_ = WallE.Wall_E2(f,df,x0,ci,cs,100)
     x_opt_GC, f0, fopt, flag,_ = WallE.Wall_E2(f,df,x0,ci,cs,100,ENABLE_GC=true)
-    #x_opt_QN, f0, fopt, flag,_ = WallE.Wall_E2(f,df,x0,ci,cs,100,ENABLE_QN=true)
  
 
     # The test
     @test isapprox(x_opt,[3.0 ; 5.0],rtol=1E-2)
     @test isapprox(x_opt_GC,[3.0 ; 5.0],rtol=1E-2)
-    #@test isapprox(x_opt_QN,[3.0 ; 5.0],rtol=1E-2)
-
+ 
     #println("\n","# Resultado #")
     #show(IOContext(stdout, :compact => false, :limit => false), "text/plain", [x_opt [3.0 ; 5.0]])
     println("\n")
@@ -67,14 +65,12 @@
     # Chama o otimizador
     x_opt, f0, fopt, flag,_ = WallE.Wall_E2(f,df,x0,ci,cs,1000)
     x_opt_GC, f0, fopt, flag,_ = WallE.Wall_E2(f,df,x0,ci,cs,1000,ENABLE_GC=true)
-    #x_opt_QN, f0, fopt, flag,_ = WallE.Wall_E2(f,df,x0,ci,cs,1000,ENABLE_QN=true)
   
 
     # The test
     @test isapprox(x_opt,[1.0 ; 3.0],rtol=1E-2)
     @test isapprox(x_opt_GC,[1.0 ; 3.0],rtol=1E-2)
-    #@test isapprox(x_opt_QN,[1.0 ; 3.0],rtol=1E-2)
-
+ 
     #println("\n","# Resultado #")
     #show(IOContext(stdout, :compact => false, :limit => false), "text/plain", [x_opt [3.0 ; 5.0]])
     println("\n")
@@ -107,14 +103,12 @@
     # Chama o otimizador
     x_opt, f0, fopt, flag,_ = WallE.Wall_E2(f,df,x0,ci,cs,1000)
     x_opt_GC, f0, fopt, flag,_ = WallE.Wall_E2(f,df,x0,ci,cs,1000,ENABLE_GC=true)
-    #x_opt_QN, f0, fopt, flag,_ = WallE.Wall_E2(f,df,x0,ci,cs,1000,ENABLE_QN=true)
  
 
     # The test
     @test isapprox(x_opt,[3.0 ; 0.5],rtol=1E-2)
     @test isapprox(x_opt_GC,[3.0 ; 0.5],rtol=1E-2)
-    #@test isapprox(x_opt_QN,[3.0 ; 0.5],rtol=1E-2)
-
+ 
 
     #println("\n","# Resultado #")
     #show(IOContext(stdout, :compact => false, :limit => false), "text/plain", [x_opt [3.0 ; 5.0]])
@@ -151,14 +145,12 @@
     # Chama o otimizador
     x_opt, f0, fopt, flag,_ = WallE.Wall_E2(f,df,x0,ci,cs,1000)
     x_opt_GC, f0, fopt, flag,_ = WallE.Wall_E2(f,df,x0,ci,cs,1000,ENABLE_GC=true)
-    #x_opt_QN, f0, fopt, flag,_ = WallE.Wall_E2(f,df,x0,ci,cs,1000,ENABLE_QN=true)
  
 
     # The test
     @test isapprox(x_opt,[0.0 ; -1.0],rtol=1E-2)
     @test isapprox(x_opt_GC,[0.0 ; -1.0],rtol=1E-2)
-    #@test isapprox(x_opt_QN,[0.0 ; -1.0],rtol=1E-2)
-
+ 
     #println("\n","# Resultado #")
     #show(IOContext(stdout, :compact => false, :limit => false), "text/plain", [x_opt [3.0 ; 5.0]])
     println("\n")
@@ -191,14 +183,12 @@
     # Chama o otimizador
     x_opt, f0, fopt, flag,_ = WallE.Wall_E2(f,df,x0,ci,cs,10_000)
     x_opt_GC, f0, fopt, flag,_ = WallE.Wall_E2(f,df,x0,ci,cs,1000,ENABLE_GC=true)
-    #x_opt_QN, f0, fopt, flag,_ = WallE.Wall_E2(f,df,x0,ci,cs,1000,ENABLE_QN=true)
  
 
     # The test
     @test isapprox(x_opt,[1.0 ; 1.0],rtol=1E-2)
     @test isapprox(x_opt_GC,[1.0 ; 1.0],rtol=1E-2)
-    #@test isapprox(x_opt_QN,[1.0 ; 1.0],rtol=1E-2)
-
+ 
     #println("\n","# Resultado #")
     #show(IOContext(stdout, :compact => false, :limit => false), "text/plain", [x_opt [3.0 ; 5.0]])
     println("\n")
