877e56001673502a0fdfee85744402536a017b9c
Eduardo Lenz <eduardobarplenz@gmail.com>
2019-09-25 16:07:06 -0300

    Fixed initial alpha and sigma (0.95)

--- src/WallE.jl
+++ src/WallE.jl
@@ -62,9 +62,9 @@ module WallE
                    flag_show::Bool=true,
                    armijo_c::Float64=0.1,
                    cut_factor::Float64=0.5,
-                   α_ini::Float64=10.0,
+                   α_ini::Float64=1.0,
                    α_min::Float64=1E-12,
-                   σ::Float64=0.999;
+                   σ::Float64=0.95;
                    STRONG::Bool=false,
                    ENABLE_GC::Bool=false)
 
