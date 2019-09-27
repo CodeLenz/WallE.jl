bffe734e32d2cf6ab5c13bc89e2bc58bae5d2fc6
Eduardo Lenz <eduardobarplenz@gmail.com>
2019-09-27 13:16:06 -0300

    Updating help and README. OWall was dropped but was still being exported.

--- src/WallE.jl
+++ src/WallE.jl
@@ -5,7 +5,7 @@ module WallE
 
   using LinearAlgebra, ProgressMeter, Dates
 
-  export Solve, Init, OWall
+  export Solve, Init
 
   # 
   # Generate the dictionary with defalt values (optional arguments)
@@ -70,29 +70,81 @@ module WallE
   ci::Array{Float64}  -> Lower side constraints
   cs::Array{Float64}  -> Upper side constraints
 
-  Optional (with default values) inputs
+  Optional (with default values) inputs are defined in a dictionary
+  with keys (and default values)
 
-  nmax_niter::Int64   -> Maximum number of iterations
-  tol_norm::Float64   -> Tolerance for the norm
-  flag_show::Bool     -> Enable/Disable printing
-  armijo_c            -> factor to evaluate Wolfe first condition
-  cut_factor::Float64 -> Factor to decrease the step length
-  α_ini::Float64      -> Initial step length
-  α_min::Float64      -> Minimum value for the step length
-  σ::Float64          -> Used to evaluate second Wolfe condition
-  strong::Bool        -> Enable strong Wolfe condition in L.S
+   "NITER"=>1000
+   "TOL_NORM"=>1E-6
+   "SHOW"=>true
+   "ARMIJO_C"=>0.1
+   "ARMIJO_TAU"=>0.5
+   "LS_ALPHA_INI"=>100.0
+   "LS_ALPHA_MIN"=>1E-12
+   "LS_SIGMA"=>0.9
+   "LS_STRONG"=>false
+   "GC"=>true
 
-  Special argument
 
-  ENABLE_GC::Bool=false
+where NITER is the number of iterations, TOL_NORM is the (relative) 
+tolerance of the norm with respect to the objective function, 
+SHOW enables a summary at the end of the optimization, 
+ARMIJO_C is the constant associated to the expected decrease of the
+objective function (first  Wolfe condition), LS_ALPHA_INI is the 
+initial step in Armijo's Backtracking line search, LS_ALPHA_MIN is
+the minimum allowable step, LS_SIGMA is the parameter associated to
+the expected decrese in curvature (second Wolfe condition) that is 
+used only if LS_STRONG is true. GC enables the (experimental) 
+constrained conjugate gradient. If it cannot be used in some iteration,
+the program automatically switch to steepest descent.
 
-  Outputs: 
+ 
+The outputs are returned in another dictionary with keys
 
-  x0::Array{Float64} -> Optimal point
-  f0::Float64        -> Initial objective function
-  fn::Float64        -> Final objective function
-  flag_conv::Bool    -> Satisfy/Do not satisfy the first order conditions
-  [functions, norms, steps] -> lists with values for each iteration
+   "RESULT"
+   "FINI"
+   "FOPT"
+   "CONVERGED"
+
+where RESULT is the vector of optimal design variables, 
+FINI is the initial value of the objective function,
+FOPT is the optimal value of the objective function and 
+CONVERGED is the flag indicating if the optimal solution 
+satisfies first order optimality conditions.
+
+Example:
+
+```julia
+    using WallE
+
+    # Objective function
+    function f(x) 
+       (x[1]+2*x[2]-7)^2 + (2*x[1]+x[2]-5)^2 
+    end
+
+    # Gradient 
+    function df(x)
+        df1 = 2*(2*x[2]+x[1]-7)+4*(x[2]+2*x[1]-5)
+        df2 = 4*(2*x[2]+x[1]-7)+2*(x[2]+2*x[1]-5)
+        return [df1 ; df2]
+    end
+
+    # Initial point
+    x0 = [-5.0 ; -5.0]
+
+    # Side constraints
+    ci =  -Inf*ones(2)
+    cs =  [0.5 ; 2.0]
+
+    # Call optimizer
+    options = WallE.Init()
+    options["NITER"] = 1000
+    output = WallE.Solve(f,df,x0,ci,cs,options)
+
+    # Recovering solution
+    x_opt = output["RESULT"]
+    flag_converged = output["CONVERGED"]
+
+```
 
   """
   function Solve(f::Function,df::Function,
